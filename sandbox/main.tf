c713a72a859717fe97a585cbea60be4471452cca
ppresto <pgpresto@gmail.com>
2019-07-17 13:52:34 -0700

    Update main.tf

--- sandbox/main.tf
+++ sandbox/main.tf
@@ -1,19 +1,6 @@
 //--------------------------------------------------------------------
-// Variables
-
-
-
-//--------------------------------------------------------------------
 // Modules
 module "instance_module" {
   source  = "app.terraform.io/ppresto_ptfe/instance-module/aws"
-  version = "1.0.1"
-
-  key_name = "${module.ssh_keypair_module.name}"
+  version = "1.0.2-keys"
 }
-
-module "ssh_keypair_module" {
-  source  = "app.terraform.io/ppresto_ptfe/ssh-keypair-module/aws"
-  version = "0.2.1"
-}
-
