730750d9fc0b589b52d465c6851cbf2edffbf24b
ppresto <ppresto@hashicorp.com>
2019-07-17 15:23:18 -0700

    cidr variables added

--- main.tf
+++ main.tf
@@ -60,35 +60,35 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.ingress_cidr_block}"]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.ingress_cidr_block}"]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.ingress_cidr_block}"]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 8800
     to_port     = 8800
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.ingress_cidr_block}"]
   }
 
   egress {
     protocol    = -1
     from_port   = 0
     to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.egress_cidr_block}"]
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -13,3 +13,15 @@ variable "instance_type" {
   type        = "string"
   default     = "t2.micro"
 }
+
+variable "egress_cidr_block" {
+  description = "Outgoing Traffic trusted locations"
+  type        = "string"
+  default     = "0.0.0.0/0"
+}
+
+variable "ingress_cidr_block" {
+  description = "Trusted Incoming Traffic cidr_block (EX: <YOUR_PUBLIC_IP>/32)"
+  type        = "string"
+  default     = "0.0.0.0/0"
+}
