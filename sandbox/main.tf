5d51881720c3dbbf264f335c6d37b96e6441c212
ppresto <ppresto@hashicorp.com>
2019-07-17 17:25:51 -0700

    removing sandbox

--- sandbox/main.tf
+++
@@ -1,8 +0,0 @@
-//--------------------------------------------------------------------
-// Modules
-module "instance_module" {
-  source  = "app.terraform.io/ppresto_ptfe/instance-module/aws"
-  version = "1.0.4-k-cidr"
-
-  ingress_cidr_block = "0.0.0.0/0"
-}
