d4d339fa55f4a60631e57f80eb21843d65796677
ppresto <ppresto@hashicorp.com>
2019-09-18 14:17:02 -0700

    updated module version

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "patrick_tf_aws_standard_network" {
 // Modules
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
-  version = "0.1.6"
+  version = "0.1.7"
   name_prefix = "${var.name_prefix}"
   security_group = "${data.terraform_remote_state.patrick_tf_aws_standard_network.security_group_web}"
 }
