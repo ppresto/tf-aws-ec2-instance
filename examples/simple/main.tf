88d6fcc434c49f2852cc62fc42febf986825b5bd
ppresto <ppresto@hashicorp.com>
2019-09-30 17:57:24 -0700

    testing count

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -13,6 +13,7 @@ data "terraform_remote_state" "patrick_tf_aws_standard_network" {
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
   version = "0.1.8"
+  count  = 1
   name_prefix = "${var.name_prefix}"
   security_group = "${data.terraform_remote_state.patrick_tf_aws_standard_network.security_group_web}"
 }
