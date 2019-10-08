652d687803a268fe0a486b56cf959a0830fd06dd
ppresto <ppresto@hashicorp.com>
2019-10-08 15:27:10 -0700

    test4

--- examples/dev/main.tf
+++ examples/dev/main.tf
@@ -1,6 +1,7 @@
 //--------------------------------------------------------------------
 // Workspace Data
-data "terraform_remote_state" "patrick_tf_aws_standard_network" {
+data "terraform_remote
+_state" "patrick_tf_aws_standard_network" {
   backend = "atlas"
   config = {
     address = "https://app.terraform.io"
@@ -14,5 +15,5 @@ module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
   version = "2.0.1"
   name_prefix = "${var.name_prefix}"
-  security_group = "${data.terraform_remote_state.patrick_tf_aws_standard_network.security_group_web}"
+  security_group = data.terraform_remote_state.patrick_tf_aws_standard_network.outputs.security_group_web
 }
