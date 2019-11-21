2639d1fb88bfb0d1f2383f5241bff93bf4f7a30e
ppresto <ppresto@hashicorp.com>
2019-11-20 20:59:37 -0800

    v2.0.5

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -11,9 +11,9 @@ data "terraform_remote_state" "patrick_tf_aws_standard_network" {
 //--------------------------------------------------------------------
 // Modules
 module "ec2_instance" {
-  #source  = "app.terraform.io/Patrick/ec2_instance/aws"
-  source  = "../../"
-  #version = "2.0.4"
+  source  = "app.terraform.io/Patrick/ec2_instance/aws"
+  #source  = "../../"
+  version = "2.0.5"
   name_prefix = "${var.name_prefix}"
   security_group = "${data.terraform_remote_state.patrick_tf_aws_standard_network.outputs.security_group_web}"
 }
